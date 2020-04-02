87613978ad44764042a15fa252fb2b1763198de2
Armand Ribouillault <armand.ribouillault@ominatechnologies.com>
2020-04-02 14:01:45 +0200

    feat: provide singleton through __new__ method

--- frozendict/FrozenDict.py
+++ frozendict/FrozenDict.py
@@ -1,16 +1,33 @@
 from __future__ import annotations
 
-from typing import TypeVar, Mapping, Dict, Optional, Iterator, Any
+from typing import Any, Dict, Iterator, Mapping, Optional, TypeVar
 
 T = TypeVar('T')
 S = TypeVar('S')
 
 
 class FrozenDict(Mapping[S, T]):
+    """
+    An immutable dictionary that implements :py:class:`typing.Mapping`
+    protocol for python 3.
+    """
     __slots__ = ['_hash_cache', '_dict']
 
     _hash_cache: Optional[int]
     _dict: Mapping[S, T]
+    _instance = None
+    _optional_keys = {'homogeneous_type', 'remove_none_value'}
+
+    def __new__(cls, *args, **kwargs):
+        """
+        Return a singleton instance if args and kwargs are empty
+        """
+        if ((len(args) == 0 or args[0] is None or len(args[0]) == 0)
+                and len(set(kwargs.keys()) - cls._optional_keys) == 0):
+            if cls._instance is None:
+                cls._instance = super(FrozenDict, cls).__new__(cls)
+            return cls._instance
+        return super(FrozenDict, cls).__new__(cls)
 
     def __init__(self,
                  value: Mapping[S, T] = None,
@@ -21,11 +38,14 @@ class FrozenDict(Mapping[S, T]):
         self._hash_cache = None
 
         def has_homogeneous_type(i) -> bool:
+            """
+            Check that the iterator contains only the same type
+            """
             iterator: Iterator[Any] = iter(i)
             first_type: Any = type(next(iterator, None))
             return all(type(x) is first_type for x in iterator)
 
-        if value is not None:
+        if value:
             if not isinstance(value, Mapping) or not isinstance(value, Dict):
                 raise TypeError
             if remove_none_value:
@@ -37,11 +57,17 @@ class FrozenDict(Mapping[S, T]):
                     raise TypeError
             self._dict = value.copy()
         elif len(kwargs) > 0:
+            buildable_kwargs = {k: v for k, v in kwargs.items()
+                                if k not in self._optional_keys}
             if remove_none_value:
-                kwargs = {k: v for k, v in kwargs.items() if v is not None}
-            if homogeneous_type and not has_homogeneous_type(kwargs.values()):
+                buildable_kwargs = {k: v for k, v in buildable_kwargs.items()
+                                    if v is not None}
+            if (homogeneous_type
+                    and (not has_homogeneous_type(buildable_kwargs.keys())
+                         or not has_homogeneous_type(
+                                buildable_kwargs.values()))):
                 raise TypeError
-            self._dict = dict(**kwargs)
+            self._dict = dict(**buildable_kwargs)
         else:
             self._dict = dict()
 
@@ -88,18 +114,21 @@ class FrozenDict(Mapping[S, T]):
         return rv
 
 
-EMPTY_FROZEN_DICT: FrozenDict = FrozenDict()
-
-
 def frozendict(value: Mapping[S, T] = None,
                *,
                homogeneous_type=False,
                remove_none_value=False,
                **kwargs) -> FrozenDict[S, T]:
-    if value is None and len(kwargs) == 0:
-        return EMPTY_FROZEN_DICT
-    else:
-        return FrozenDict(value,
-                          homogeneous_type=homogeneous_type,
-                          remove_none_value=remove_none_value,
-                          **kwargs)
+    """
+    Lowercase alias for the FrozenDict constructor
+    :param value: a mutable dict to froze
+    :param homogeneous_type: check that the content of the dictionary has
+    homogeneous types for key and value.
+    :param remove_none_value: remove any None value from the dictionary
+    :param kwargs: It is possible to create the instance through kwargs
+    :return:
+    """
+    return FrozenDict(value,
+                      homogeneous_type=homogeneous_type,
+                      remove_none_value=remove_none_value,
+                      **kwargs)
