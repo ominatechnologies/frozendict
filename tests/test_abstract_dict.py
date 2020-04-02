cc0285f784964b3dd7eb84d5bc72a74b979f9cf9
Wouter Van den Broeck <wouter@addith.be>
2020-04-02 13:27:23 +0200

    test: add AbstractDict typing tests (which currently fail)

--- tests/test_abstract_dict.py
+++ tests/test_abstract_dict.py
@@ -13,3 +13,16 @@ def test_isinstance():
     d = {'test': 0}
     assert isinstance(d, Mapping)
     assert isinstance(d, AbstractDict)
+
+
+def test_abstract_dict_typing_1():
+    dict()
+    dct: AbstractDict[str, str] = {'k_1': 'v_1'}
+    assert dct['k_1'] == 'v_1'
+
+
+def test_abstract_dict_typing_2():
+    def foo(dct: AbstractDict[str, str]):
+        assert dct['k_1'] == 'v_1'
+
+    foo({'k_1': 'v_1'})
