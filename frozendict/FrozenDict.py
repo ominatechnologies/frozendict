bf5e12d0eabd8462618b8c8846e76eacf26c4953
Armand Ribouillault <armand.ribouillault@ominatechnologies.com>
2020-04-14 09:58:55 +0200

    fix: checking if the given value is hashable in the constructor

--- frozendict/FrozenDict.py
+++ frozendict/FrozenDict.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import copy
 from typing import (Any, ClassVar, Dict, Iterator, Mapping, Optional, Set,
                     TypeVar)
 
@@ -50,19 +49,21 @@ class FrozenDict(Mapping[KT, VT_co]):
                  no_copy: bool = False,
                  **kwargs):
         """
-        Instantiate a FrozenDict
-        :param value: A mapping from witch to create a FrozenDict
+        Instantiate a FrozenDict.
+
+        :param value: A mapping from witch to create a FrozenDict.
         :param homogeneous_type: Option to check that all types in
-        the dictionary are homogeneous (keys have the same type and value
-        have the same type)
+            the dictionary are homogeneous (keys have the same type and value
+            have the same type).
         :param remove_none_value: Option to remove any None value from the
-        given mapping
+            given mapping.
         :param no_copy: Option to disable the deep copy of the given mapping
-        (example: if you create a Frozendict like frozendict({k1:v1, k2:v2})
-        you don't need to copy it as there is no refrence to that mapping
-        outside)
+            (example: if you create a Frozendict like frozendict({k1:v1,
+            k2:v2})
+            you don't need to copy it as there is no refrence to that mapping
+            outside).
         :param kwargs: You can use kwargs to instantiate a Frozendict (
-        Example: Frozendict(k1:v1, k2:v2))
+            Example: Frozendict(k1:v1, k2:v2)).
         """
         self._hash_cache = None
 
@@ -75,6 +76,9 @@ class FrozenDict(Mapping[KT, VT_co]):
             return all(type(x) is first_type for x in iterator)
 
         if value is not None:
+            # The given value should be hashable
+            assert isinstance(self._hash(value), int)
+
             if not isinstance(value, Dict):
                 if isinstance(value, Mapping):
                     value = dict(value)
@@ -92,7 +96,7 @@ class FrozenDict(Mapping[KT, VT_co]):
             if no_copy:
                 self._dict = value
             else:
-                self._dict = copy.deepcopy(value)
+                self._dict = value.copy()
         elif len(kwargs) > 0:
             buildable_kwargs = {k: v for k, v in kwargs.items()
                                 if k not in self._optional_keys}
@@ -143,8 +147,20 @@ class FrozenDict(Mapping[KT, VT_co]):
     def __hash__(self):
         if self._hash_cache is not None:
             return self._hash_cache
-        rv = self._hash_cache = hash(frozenset(self._dict.items()))
+        rv = self._hash_cache = self._hash(self._dict)
         return rv
+
+    def _hash(self, o) -> int:
+        """
+          Create a hash from a dictionary, list, tuple or set. The given
+          value can be of nested type.
+          """
+        if isinstance(o, (set, tuple, list)):
+            return hash(sorted([self._hash(e) for e in o]))
+        elif not isinstance(o, dict):
+            return hash(o)
+        new_o = {k: self._hash(v) for k, v in o.items()}
+        return hash(tuple(frozenset(sorted(new_o.items()))))
 
     def __iter__(self):
         return iter(self._dict)
