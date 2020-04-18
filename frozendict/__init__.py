10b026cc31d423c95d7188f1c14bdd942c2b2e77
Wouter Van den Broeck <wouter@addith.be>
2020-04-18 10:34:47 +0200

    style: format

--- frozendict/__init__.py
+++ frozendict/__init__.py
@@ -3,9 +3,9 @@ from typing import Mapping as AbstractDict
 from .frozen_dict import FrozenDict, frozendict
 from .no_copy_frozen_dict import NoCopyFrozenDict
 
-__all__ = [
+__all__ = (
     'AbstractDict',
     'FrozenDict',
     'frozendict',
     'NoCopyFrozenDict',
-]
+)
