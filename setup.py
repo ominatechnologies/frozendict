3ac29d834e0a8563e0057eefc905d646befaa571
Armand Ribouillault <armand.ribouillault@ominatechnologies.com>
2021-09-01 11:15:25 +0200

    infra: update tox

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
 import pathlib
 
 import pkg_resources
-from config import author, author_email, description, name, repo
-from setuptools import find_packages, setup
-
-
-def readme():
-    with open("README.rst") as f:
-        return f.read()
-
+from setuptools import setup
 
 # 3rd-party run-time requirements:
 with pathlib.Path('requirements.txt').open() as requirements_txt:
@@ -18,33 +11,4 @@ with pathlib.Path('requirements.txt').open() as requirements_txt:
         in pkg_resources.parse_requirements(requirements_txt)
     ]
 
-setup(
-    author=author,
-    author_email=author_email,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    data_files=[
-        ("", ["AUTHORS.rst", "CHANGELOG.rst", "LICENSE", "README.rst"])
-    ],
-    description=description,
-    include_package_data=True,
-    install_requires=install_requires,
-    keywords=[
-        "immutable",
-        "python",
-        "type system",
-    ],
-    license="MIT",
-    long_description=readme(),
-    name=name,
-    package_data={
-        name: ["py.typed"],
-    },
-    packages=find_packages(),
-    python_requires=">=3.8.7",
-    url=repo,
-    zip_safe=False,
-)
+setup(install_requires=install_requires)
