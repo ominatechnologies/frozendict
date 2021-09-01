bb633daf8c1fa832ea214ce6698c4d10e1ddb81c
Armand Ribouillault <armand.ribouillault@ominatechnologies.com>
2021-09-01 12:10:30 +0200

    infra: fix auto tagging

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 # Single-sourced project configuration values:
 
+# The full version, including alpha/beta/rc tags:
+release = "2021.08.04"
+version = release
+
 # Distribution package name:
 name = "frozendict"
 
