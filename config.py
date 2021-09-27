311d436933a60086fc038e2bd97c9e01a8f25bc7
armrib <armrib@users.noreply.github.com>
2021-09-27 14:58:28 +0000

    chore: Release 2021.9.27.3

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Single-sourced project configuration values:
 
 # The full version, including alpha/beta/rc tags:
-release = "2021.9.27.2"
+release = "2021.9.27.3"
 version = release
 
 # Distribution package name:
