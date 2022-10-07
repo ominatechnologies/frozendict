b7156b5c48599a2bbcb8b5824784cbec9e4ebad9
Armand Ribouillault <armand.ribouillault@ominatechnologies.com>
2022-10-07 14:01:35 +0200

    chore: remove old setup and enforce code quality

--- src/frozendict/__init__.py
+++ src/frozendict/__init__.py
@@ -2,10 +2,3 @@ from typing import Mapping as AbstractDict
 
 from .frozen_dict import FrozenDict, frozendict
 from .no_copy_frozen_dict import NoCopyFrozenDict
-
-__all__ = [
-    "AbstractDict",
-    "FrozenDict",
-    "frozendict",
-    "NoCopyFrozenDict",
-]
